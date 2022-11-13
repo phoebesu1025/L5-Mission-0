7e72dd073fec1fa969513bf05770240956c70fa5
phoebesu1025 <phoebesu85@yahoo.com.tw>
2022-11-13 13:46:22 +1300

    setting up the components in App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "../src/Components/Header"
+import TopSection from "../src/Components/TopSection"
+import PictureGrid from "../src/Components/PictureGrid"
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <TopSection />
+      <PictureGrid />
+    </>
+  )
 }
 
 export default App;
